31650b7bf7781ccc8a6819968fbf9aeb20703432
nlakshminarasimhamur <nlakshminarasimhamur@LV-IND-LPT-226.corp.TFHNET.LOCAL>
2020-01-16 13:02:20 +0530

    hiii

--- BankApk/src/main/java/emailCreationApplicatrion/Accounting.java
+++ BankApk/src/main/java/emailCreationApplicatrion/Accounting.java
@@ -2,4 +2,9 @@ package emailCreationApplicatrion;
 
 public class Accounting extends GeneralEmail {
 
+	public Accounting(String firstname, String lastName, String department, String company) {
+		super(firstname, lastName, department, company);
+		// TODO Auto-generated constructor stub
+	}
+;
 }
